5dcd407eed1ef1d72040bd029b46e5646eff8830
Skyba123 <skyba.serhi@gmail.com>
2023-09-23 20:26:52 +0300

    added method findBookByTitle, added  method removeBookByIsbn

--- LibraryManagement.java
+++ LibraryManagement.java
@@ -51,7 +51,27 @@ class Library {
         }
     }
 
+     public Book findBookByTitle(String title) {
+        for (Book book : books) {
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                return book;
+            }
+        }
+        return null;
+    }
 
-
+      public void removeBookByIsbn(String isbn) {
+        Iterator<Book> iterator = books.iterator();
+        while (iterator.hasNext()) {
+            Book book = iterator.next();
+            if (book.getIsbn().equals(isbn)) {
+                iterator.remove();
+                System.out.println("Book with ISBN " + isbn + " has been removed from the library.");
+                return;
+            }
+        }
+        System.out.println("Book with ISBN " + isbn + " not found in the library.");
+    }
+  }
 
 }
