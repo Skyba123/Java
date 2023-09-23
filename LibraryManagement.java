0d592a11660838db53e6a2a5e7f749b0e33a2659
Skyba123 <skyba.serhi@gmail.com>
2023-09-23 19:50:55 +0300

    Created the class Book, added fields, added a constructor

---
+++ LibraryManagement.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+class Book {
+    private String title;
+    private String author;
+    private String isbn;
+    private int publicationYear;
+
+    public Book(String title, String author, String isbn, int publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.publicationYear = publicationYear;
+    }
+}
